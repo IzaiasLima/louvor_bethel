ed94f24e9e1ffd817be7c4275b1c9cb858ccfed2
IzaiasLima <izaiasmlima@gmail.com>
2021-05-09 01:58:25 -0300

    Inicio da definição do thema a ser usado no app

---
+++ lib/common/theme.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+final double fonteSize = 12.0;
+
+final MaterialColor swatchColor = Colors.indigo;
+
+final Color backColor = Color.fromARGB(255, 255, 250, 250);
+
+final ThemeData appTheme = ThemeData(
+  primarySwatch: swatchColor,
+  primaryColor: Color.fromARGB(255, 130, 130, 135),
+  scaffoldBackgroundColor: backColor,
+);
