9e5c68857d9b99fc50fc1b89dc2b017020e7c187
IzaiasLima <izaiasmlima@gmail.com>
2021-05-09 18:01:21 -0300

    Iniciando a Homepage.

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -9,18 +9,37 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      drawer: Drawer(),
       appBar: AppBar(
         actions: [
           Icon(Icons.home),
         ],
       ),
-      body: Container(
-        child: Text('Homepage'),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () => null,
-        child: Icon(Icons.add),
-      ),
+      body: Column(children: [
+        Column(children: [
+          Container(
+            child: Text('Semana 21/03 a 26/03'),
+          ),
+          Divider(color: Colors.black),
+        ]),
+        Row(
+          children: [
+            Text('Domingo 18h30'),
+            SizedBox(width: 8),
+            Text('Celebração'),
+          ],
+        ),
+        Card(
+          child: Column(children: [
+            Text('Ao único que é digno'),
+            Text('Grande é o Senhor'),
+            Text('Tua mão forte'),
+            Text('Que se abram os céus'),
+            Text('Ele é exaltado'),
+            Text('Yeshua'),
+          ]),
+        ),
+      ]),
     );
   }
 }
